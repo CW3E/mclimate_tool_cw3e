ae3f7e2a32238e4035ba4ba57fdb6ce97d31ef96
Deanna Nash <dnash@ucsd.edu>
2024-09-05 10:30:32 -0700

    change path to mclimate data on skyriver

--- mclimate_funcs.py
+++ mclimate_funcs.py
@@ -87,10 +87,10 @@ def load_mclimate(mon, day, varname, server):
         
     ## load mclimate data
     if server == 'skyriver':
-        path_to_data = '/home/dnash/comet_data/' # skyriver
+        path_to_data = '/data/projects/operations/GEFS_mclimate/data/' # skyriver
     elif server == 'expanse':
-        path_to_data = '/expanse/nfs/cw3e/cwp140/' # expanse 
-    fname = path_to_data + 'preprocessed/{2}_mclimate/GEFSv12_reforecast_mclimate_{2}_{0}{1}.nc'.format(mon, day, varname)
+        path_to_data = '/expanse/nfs/cw3e/cwp140/preprocessed/' # expanse 
+    fname = path_to_data + '{2}_mclimate/GEFSv12_reforecast_mclimate_{2}_{0}{1}.nc'.format(mon, day, varname)
     # print(fname_pattern)
     ds = xr.open_dataset(fname)
     # ds = ds.sortby("step") # sort by step (forecast lead)
