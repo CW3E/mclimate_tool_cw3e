6b65a2d9ae8971a9a91cb7ef349c721b6203a028
Deanna Nash <dnash@ucsd.edu>
2025-03-05 12:11:16 -0800

    add trailing slash to fig path

--- run_tool_four-panel.py
+++ run_tool_four-panel.py
@@ -43,7 +43,7 @@ remove_tmp_data_files(directory, pattern)
 ### VARS TO UPDATE ###
 ######################
 model = 'GEFS' ## 'GEFSv12_reforecast', 'GFS', 'GEFS', 'GEFS_archive'
-fig_path = '/data/projects/operations/GEFS_Mclimate/figs/images_operational'
+fig_path = '/data/projects/operations/GEFS_Mclimate/figs/images_operational/'
 # os.makedirs(os.path.dirname(fig_path), exist_ok=True)
 
 ################
