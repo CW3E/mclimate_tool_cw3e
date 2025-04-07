88947f00a90bff98d4cc6bc94e60a32186362a53
Deanna Nash <dnash@exp-18-04.expanse.sdsc.edu>
2025-04-07 10:52:15 -0700

    add trailing slash

--- run_tool.py
+++ run_tool.py
@@ -42,7 +42,7 @@ remove_tmp_data_files(directory, pattern)
 ######################
 model = 'GEFS' ## 'GEFSv12_reforecast', 'GFS', 'GEFS', 'GEFS_archive'
 # fig_path = '/data/projects/operations/GEFS_Mclimate/figs/images_operational/'
-fig_path = '/data/projects/website/mirror/htdocs/Projects/MClimate/images/images_operational'
+fig_path = '/data/projects/website/mirror/htdocs/Projects/MClimate/images/images_operational/'
 # os.makedirs(os.path.dirname(fig_path), exist_ok=True)
 
 ################
